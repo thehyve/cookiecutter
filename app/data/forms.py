87097d7b385e0e181cc8d8c96b4c1410e8a067c8
Piotr Zakrzewski <piotr@thehyve.nl>
2017-04-03 17:54:29 +0200

    Transmart configuration draft

---
+++ app/data/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import Form
+from wtforms import ValidationError
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+
+class ChangeTransmartUrl(Form):
+    url = StringField(
+        'New URL', validators=[InputRequired(), Length(1, 128)])
+    submit = SubmitField('Update url')
+
+
+
+
+class ChangeTransmartVersion(Form):
+    version = StringField(
+        'New Version', validators=[InputRequired(), Length(1, 128)])
+    submit = SubmitField('Update role')
